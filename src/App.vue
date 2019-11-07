ce4525edad06c75fc4db351ae9e4ce7f300f189c
Edison <edirates@gmail.com>
2019-11-07 18:24:09 +0700

    Standard login page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
+    <loginpage></loginpage>
     <mainpage></mainpage>
   </div>
 </template>
 
 <script>
 import mainpage from "./view/mainpage";
+import loginpage from "./view/loginpage"
 
 export default {
   name:'App',
   components:{
-    mainpage
+    mainpage,
+    loginpage
   },
   data: function(){
-    return {
-      hello: 'haii'
-    }
+   return {
+     hello: ''
+   }
   }
 }
 </script>
---
+++ src/components/register.vue
@@ -0,0 +1,44 @@
+<template>
+    <form @submit.prevent="login()" method="POST">
+        <b-field 
+            label="Email"
+            :type="flagError"
+            :message="errorMessage">
+            <b-input 
+                type="email"
+                placeholder="jonsnow@stark.com"
+                maxlength="30"
+                v-model="email">
+            </b-input>
+        </b-field>
+        <b-field 
+            label="Username"
+            :type="flagError"
+            :message="errorMessage">
+            <b-input 
+                placeholder="jonsnow" 
+                maxlength="30"
+                v-model="username">
+            </b-input>
+        </b-field>
+        <b-field label="Password">
+            <b-input 
+                type="password"
+                placeholder="*******"
+                password-reveal
+                v-model="password">
+            </b-input>
+        </b-field>
+        <b-button type="is-primary">Login</b-button>
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
