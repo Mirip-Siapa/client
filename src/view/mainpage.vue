0231b1b7c9431f3e378501dd6390def6ff36717a
akbfitra <akbarfitralistyono@gmail.com>
2019-11-07 18:16:41 +0700

    ?

---
+++ src/view/mainpage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="container is-fluid">
+    <navbar></navbar>
+      <div class="columns">
+        <div class="column is-3 box">
+          Left Menu
+        </div>
+        <div class="column box">
+          <card></card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '../components/card';
+import navbar from '../components/navbar';
+export default {
+  name: 'mainpage',
+  components:{
+    card,
+    navbar
+  },
+  data: function(){
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  container{
+    margin-top: 200px;
+  }
+</style>
